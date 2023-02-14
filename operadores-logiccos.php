d05a64028bd21c6d9e544b4e75cf2c3055667286
Carolina2118 <loboscarolinaa@gmail.com>
2023-02-14 18:08:15 -0300

    operadores lógicos- var_dump

--- operadores-logiccos.php
+++ operadores-logiccos.php
@@ -3,7 +3,10 @@
   $valorA = true;
   $valorB = true;
   
-  $result = ($valorA and $valorB);  // comparar las variables entre parentesis.
-  var_dump($result);
-
+  $resultado = ($valorA and $valorB);  
+   // comparar las variables entre parentesis.
+var_dump($resultado);
+/* ojo :La función var_dump() muestra el tipo de variable y su contenido, en el caso de los arrays muestra el tipo de contenido de cada campo. 
+Es muy parecida a la función var_export(),
+la diferencia radica en que esta última muestra el mismo resultado pero en código PHP interpretable.*/
 ?>
