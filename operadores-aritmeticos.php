165202bf6daa01cc37f479de73a4859f2bc2c28b
Carolina2118 <loboscarolinaa@gmail.com>
2023-02-16 18:28:17 -0300

    operadores aritsmeticos

---
+++ operadores-aritmeticos.php
@@ -0,0 +1,23 @@
+<?php 
+$valorA = 5;
+$valorB = 2;
+
+$suma = $valorA + $valorB;
+var_dump($suma);
+
+$resta = $valorA - $valorB;
+var_dump($resta);
+
+$multiplicacion = $valorA * $valorB;
+var_dump($multiplicacion);
+
+$dividir = $valorA / $valorB;
+var_dump($dividir);
+
+$modulo = $valorA % $valorB;
+var_dump($modulo);
+
+$potenciacion = $valorA ** $valorB;
+// 5 x 5 = 25 
+var_dump($potenciacion);
+?>
