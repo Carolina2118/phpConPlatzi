6c130fd7e502e9fd6c0dd3d84fc361a7e5fd8d4c
Carolina2118 <loboscarolinaa@gmail.com>
2023-02-18 19:06:44 -0300

    if-else while- do while

---
+++ estructura-de-color/if-Else-while.php
@@ -0,0 +1,19 @@
+<?php 
+$valorA =5;
+
+if ($valorA == 10){
+    echo "Es igual a 10";
+} else {
+    echo "No es igual a 10";
+}
+
+$numero = 0;
+while($numero <=10){
+     echo $numero++;
+}
+
+$numeroli = 0;
+do { 
+    echo $numeroli++;
+}while ($numeroli <= 10);
+?>
