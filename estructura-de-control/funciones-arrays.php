1896127096b94073d00f4444e4426311d4d99446
Carolina2118 <loboscarolinaa@gmail.com>
2023-03-01 15:09:05 -0300

    max,mini y numero aleatorio con rand

--- estructura-de-control/funciones-arrays.php
+++ estructura-de-control/funciones-arrays.php
@@ -1,9 +1,11 @@
 <?php 
 #aca le indicamos que es un ARRAY . Y SU RESPUESTA ES TRUE
+
 $nombre = array("Ana","Juan","Camila");
 var_dump(is_array($nombre));
 
 # aca indicamos que no es un array pero pedimos que nos de el resultado de un array y es false 
+
 $nombre1 ="Ana";
 var_dump(is_array($nombre1));
 
---
+++ estructura-de-control/funciones-matematicas.php
@@ -0,0 +1,9 @@
+<?php
+//max muestra el valor más alto 
+echo max (9,4,3,2,6);
+// min muestra el valor más bajo o minimo 
+echo min(9,4,3,2,6);
+
+#ramd muestra un numero a liatorio , caba ves que se consulte sera un numero distinto 
+echo rand(40,100);
+?>
