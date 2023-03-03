b6c91f0de8da529b737e3f4b4a0d378f0040c3af
Carolina2118 <loboscarolinaa@gmail.com>
2023-03-03 15:30:14 -0300

    ejercico

---
+++ ejercicios/ejercicioUno.php
@@ -0,0 +1,12 @@
+<?php 
+
+$estudiante = 7;
+
+if ($estudiante <=5 ) {
+    echo "El estudiante tendrá sus juguetes en la parte inferior de la bodega.";
+
+} if ($estudiante >=7){
+echo "El estudiante tendrá sus juguetes en la parte media de la bodega.";
+} 
+
+?>
--- estructura-de-control/if-Else-while.php
+++ estructura-de-control/if-Else-while.php
@@ -1,7 +1,7 @@
 <?php 
-$valorA =5;
+$valorA =7;
 
-if ($valorA == 10){
+if ($valorA <= 10){
     echo "Es igual a 10";
 } else {
     echo "No es igual a 10";
