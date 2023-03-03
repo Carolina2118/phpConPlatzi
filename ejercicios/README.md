d64ddde498ec01b055b9401cb350bc93260f5d1e
Carolina2118 <loboscarolinaa@gmail.com>
2023-03-03 17:45:48 -0300

    conto pero esta echo

--- ejercicios/README.md
+++ ejercicios/README.md
@@ -3,8 +3,11 @@
  - En una escuela están almacenando los juguetes favoritos de los estudiantes de acuerdo a su edad en un lugar específico de la bodega de esta forma:
 
     Quienes sean menores de 5 años tendrán sus espacios en la parte inferior de la bodega.
+
     Quienes tengan entre 5 y 7 años tendrán sus espacios en la parte media de la bodega.
+
     Quienes tengan más de 7 años tendrán sus espacios en la parte alta de la bodega.
+    
     Quienes no tengan registro de su edad aún tendrán un espacio de almacenamiento en la bodega de al lado.
 
 Según una lista que contiene las edades de cada estudiante debes validar su edad y mostrar a través de un mensaje en dónde tendrán que almacenar sus juguetes.
--- ejercicios/ejercicioUno.php
+++ ejercicios/ejercicioUno.php
@@ -1,12 +1,33 @@
 <?php 
+/*--
+$estudiante = 0;
 
-$estudiante = 7;
-
-if ($estudiante <=5 ) {
+if ($estudiante >=4) {
     echo "El estudiante tendrá sus juguetes en la parte inferior de la bodega.";
 
 } if ($estudiante >=7){
 echo "El estudiante tendrá sus juguetes en la parte media de la bodega.";
 } 
+
+?>
+
+*/
+?>
+<?php
+$student =5;
+switch ($student) {
+    case  ($student <=5):
+    echo "Tendrás tu espacios en la parte inferior de la bodega.";
+    break;
+    case($student >5 && $student <7):
+          echo "Tendrán sus espacios en la parte media de la bodega.";
+        break;
+        case ($student >=7):
+        echo "Tendrán sus espacios en la parte alta de la bodega.";
+            break;
+    default:
+        echo" No tengan registro de su edad pero igual tendrán un espacio de almacenamiento en la bodega de al lado.";
+        break;
+}
 
 ?>
