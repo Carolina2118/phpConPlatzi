3269ee7bbb0c19d96f3e27d06abd27d57f3a230c
Carolina2118 <loboscarolinaa@gmail.com>
2023-03-07 18:44:42 -0300

    creando la respuesta del formulario

--- Microproyecto/main.php
+++ Microproyecto/main.php
@@ -1,6 +1,44 @@
 <?php
 
+$palabras = array("Sombra","Raton", "Girasol");
+$palabrasDesordenadas =array();
+
+for ($i=0; $i < 3; $i++) { 
+    
+   $palabrasDesordenadas[$i] = str_shuffle($palabras[$i]);
+}
+
+print_r($palabrasDesordenadas);
 
 
+echo"
+<form action='analisis.php' style = 'padding-left: 270px;
+padding-right: 270px;'>
+<div style ='
+
+background: linear-gradient(to right, #093637, #44A08D); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+'>
+<p>Te dare una pista.</p>
+<p> En el jardín estoy y en tus pies siempre voy.</p>
+<P>ARBMOS </P>
+
+<input type='text' name='palabra0' style='border-radius: 5px;'>
+
+<p>Hay quienes huyen de mí por temor y quienes me conectan en el ordenador</P>
+<P>ONTRA</P>
+
+<input type='text' name='palabra1' style='border-radius: 5px;'>
+
+<p>Siempre mirando al sol y no soy un caracol. Giro y giro sin fin y no soy un bailarín.</p>
+<p> SORALGI</p>
+
+<input type='text' name='palabra2' style='border-radius: 5px;'>
+
+<br><br>
+
+<button type='submit' style='background-color:rgb(49, 179, 31); margin-left: 54px;border-radius: 6px;'>Enviar</button>
+</div>
+</form>
+";
 
 ?>
