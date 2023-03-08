9eeb191afce78c605d6e3c999b56889596d86de5
Carolina2118 <loboscarolinaa@gmail.com>
2023-03-08 12:19:48 -0300

    terminado

--- Microproyecto/analisis.php
+++ Microproyecto/analisis.php
@@ -3,22 +3,22 @@ print_r($_REQUEST);
 
 $palabras = array("sombra","raton", "girasol");
 if ($_REQUEST["palabra0"] == $palabras[0]) {
-    echo "La palabra ingresada es correcta ";
+    echo "La palabra ingresada es correcta: ";
 }else {
-    echo"La palabra ingresada es incorrecta".$palabras[0];
+    echo"La palabra ingresada es incorrecta: ".$palabras[0];
 }
 
 if ($_REQUEST["palabra1"] == $palabras[1]) {
-    echo "La palabra ingresada es correcta ";
+    echo "La palabra ingresada es correcta: ";
 }else {
-    echo"La palabra ingresada es incorrecta".$palabras[1];
+    echo"La palabra ingresada es incorrecta: ".$palabras[1];
 }
 if ($_REQUEST["palabra2"] == $palabras[2]) {
-    echo "La palabra ingresada es correcta ";
+    echo "La palabra ingresada es correcta:  ".$palabras[2];
 }else {
     
+    echo"La palabra ingresada es incorrecta: ".$palabras[2];
 }
-echo"La palabra ingresada es incorrecta".$palabras[2];
 
 
 
--- Microproyecto/main.php
+++ Microproyecto/main.php
@@ -12,30 +12,29 @@ print_r($palabrasDesordenadas);
 
 
 echo"
-<form action='analisis.php' style = 'padding-left: 270px;
-padding-right: 270px;'>
-<div style ='background: linear-gradient(to right, #093637, #44A08D);'>
-<p>Te dare una pista.</p>
-<p> En el jardín estoy y en tus pies siempre voy.</p>
-<P>ARBMOS </P>
+<div style='border-style: double; padding: 10px; border: 100px solid #adabaa; background-color: #d79448;'>
 
-<input type='text' name='palabra0' style='border-radius: 5px;'>
+<p style =' text-align: center;font-size: 40px'>Adivina cual es la palabra</p>
 
-<p>Hay quienes huyen de mí por temor y quienes me conectan en el ordenador</P>
-<P>ONTRA</P>
+    <form action='analisis.php'>
+        <div style='display:flex; flex-direction:column;  padding: 10px; border: 9px solid #4CAF50; align-items: center;'>
+            <p style='flex-direction:column;text-align: center; font-size: 20px;'> En el jardín estoy y en tus pies siempre voy.</p>
+                <P style='text-align: center;font-size: 25px;' >ARBMOS </P>
+                <input type='text' name='palabra0' style='flex-direction:column;border-radius: 5px;width: 15em; padding: 4px;'>
+            <p style='text-align: center;font-size: 20px;'>Hay quienes huyen de mí por temor y quienes me conectan en el ordenador</P>
 
-<input type='text' name='palabra1' style='border-radius: 5px;'>
+            <P style='text-align: center;font-size: 25px;'>ONTRA</P>
+                <input type='text'name='palabra1' style='border-radius: 5px;width: 15em; padding: 4px;'>
+            <p style='text-align: center;font-size: 20px;'>Siempre mirando al sol y no soy un caracol.
+                Giro y giro sin fin y no soy un bailarín.</p>
 
-<p>Siempre mirando al sol y no soy un caracol. Giro y giro sin fin y no soy un bailarín.</p>
-<p> SORALGI</p>
-
-<input type='text' name='palabra2' style='border-radius: 5px;'>
-
-<br><br>
-
-<button type='submit' style='background-color:rgb(49, 179, 31); margin-left: 54px;border-radius: 6px;'>Enviar</button>
+            <p style='text-align: center;font-size: 25px;'> SORALGI</p>
+                <input type='text' name='palabra2' style='border-radius: 5px;width: 15em; padding: 4px;'>
+            <br><br>
+                <button type='submit' style='justify-content: center; margin-top: 48px; background-color:rgb(49, 179, 31); border-radius: 6px; width:10em; padding: 6px;font-size: 20px;'>Enviar</button>
+        </div>
+    </form>
 </div>
-</form>
 ";
 
 ?>
