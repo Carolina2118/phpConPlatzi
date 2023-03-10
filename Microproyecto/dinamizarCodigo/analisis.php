e17b7029cf66475508036bfb67ffefcd6f8c3022
Carolina2118 <loboscarolinaa@gmail.com>
2023-03-10 19:01:33 -0300

    juego completado

--- Microproyecto/dinamizarCodigo/analisis.php
+++ Microproyecto/dinamizarCodigo/analisis.php
@@ -1,24 +1,17 @@
 <?php
-print_r($_REQUEST);
+#print_r($_REQUEST);
 
-$palabras = array("sombra","raton", "girasol");
-if ($_REQUEST["palabra0"] == $palabras[0]) {
-    echo "La palabra ingresada es correcta: ";
-}else {
-    echo"La palabra ingresada es incorrecta: ".$palabras[0];
+$palabras = array("sol", "luna", "cielo", "tierra", "agua", "estrellas");
+
+for ($i = 0; $i < count($palabras); $i++) {
+    if ($_REQUEST["palabra" . $i] == $palabras[$i]) {
+        echo "La palabra ingresada es correcta:" . "<br>";
+    } else {
+        echo "La palabra ingresada es incorrecta, la palabra correcta es: " .
+            $palabras[$i] . "<br>";
+    }
 }
 
-if ($_REQUEST["palabra1"] == $palabras[1]) {
-    echo "La palabra ingresada es correcta: ";
-}else {
-    echo"La palabra ingresada es incorrecta: ".$palabras[1];
-}
-if ($_REQUEST["palabra2"] == $palabras[2]) {
-    echo "La palabra ingresada es correcta:  ".$palabras[2];
-}else {
-    
-    echo"La palabra ingresada es incorrecta: ".$palabras[2];
-} 
+
 
 ?>
-
--- Microproyecto/dinamizarCodigo/main.php
+++ Microproyecto/dinamizarCodigo/main.php
@@ -1,16 +1,24 @@
 <?php
-$palabras =array("sol","luna","cielo","tierra","agua", "estrelllas");
-    $form ="<from action='analisis.php'>";
+$palabras =array("sol","luna","cielo","tierra","agua", "estrellas");
+
+$form = "<div style='border-style: double; padding: 10px; border: 100px solid #adabaa; background-color: #d79448;'> 
+<p style =' text-align: center;font-size: 40px'>Adivina cual es la palabra</p>
+  <form action='analisis.php'> <div style='display:flex; flex-direction:column;  padding: 10px; border: 9px solid #4CAF50; align-items: center;'> ";
+
+for ($i=0 ; $i < count($palabras); $i++ ) {
+    $form .= "La palabra: ".str_shuffle($palabras[$i])." ". 
+    "<input type='text' name='palabra".$i."'>".
+    "<br>"; 
     
-    for ($i=0 ; $i < count($palabras); $i++ ) {
-        $form .= "La palabra: ".str_shuffle($palabras[$i])." "."<br>"."<br>".
-        "<input type='text' name='palabra ".$i."'>".
-        "<br>"."<br>"; 
-        
-    }
-$button = "<button type='submit'>Enviar</button>";
-$cierreForm="</from>";
+}
+
+$button = "<button type='submit' style='justify-content: center; margin-top: 48px;
+ background-color:rgb(49, 179, 31); border-radius: 6px;
+ width:10em; padding: 6px;font-size: 20px;'>Enviar</button>";
+$cierreForm = "</from>";
 
 echo $form.$button.$cierreForm;
+
+
 
 ?>
